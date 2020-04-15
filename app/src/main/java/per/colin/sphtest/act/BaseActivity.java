edeac53e1ddda7595fcc670c7f4a893165e367ee
ColinJiang <pahu2000@gmail.com>
2020-04-15 21:54:06 +0800

    Initial commit

---
+++ app/src/main/java/per/colin/sphtest/act/BaseActivity.java
@@ -0,0 +1,6 @@
+package per.colin.sphtest.act;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public abstract class BaseActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/per/colin/sphtest/act/MainActivity.java
@@ -0,0 +1,9 @@
+package per.colin.sphtest.act;
+
+/**
+ * @author pahu2000
+ */
+public class MainActivity extends BaseActivity {
+
+    
+}
