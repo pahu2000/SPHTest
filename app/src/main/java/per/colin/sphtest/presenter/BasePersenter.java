03f92c5deeef092d0c77fe4e6efd044941aa56f4
ColinJiang <pahu2000@gmail.com>
2020-04-23 15:53:49 +0800

    删除错误的类

--- app/src/main/java/per/colin/sphtest/presenter/BasePersenter.java
+++
@@ -1,5 +0,0 @@
-package per.colin.sphtest.presenter;
-
-public abstract class BasePersenter {
-
-}
--- app/src/main/java/per/colin/sphtest/presenter/MainPersenter.java
+++
@@ -1,5 +0,0 @@
-package per.colin.sphtest.presenter;
-
-public class MainPersenter extends BasePersenter {
-
-}
